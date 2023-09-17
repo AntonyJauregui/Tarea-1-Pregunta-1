84355667dc1a95114aabe887362b4dde884adf3f
AntonyJauregui <139800467+AntonyJauregui@users.noreply.github.com>
2023-09-17 01:05:26 -0500

    2023.09.17

---
+++ src/ColorChanger.jsx
@@ -0,0 +1,6 @@
+export const ColorChanger = () => {
+    return (
+            <div className="cuadrado">
+                <img src="" alt="" width={100} height={100} border={1} />
+            </div>
+    )}
